6417ffb2dfe9c59c3dcb78314b702f2b774a5ffb
zhouxiaotian <1282347298@qq.com>
2017-08-17 18:39:07 +0800

    20170817

--- WEEK4/BANNER/js/banner1.js
+++ WEEK4/BANNER/js/banner1.js
@@ -29,13 +29,8 @@ var banner = document.querySelector('.banner'),
     imgBox.innerHTML = str;
     focus.innerHTML = strFocus;
 
-    //->为了实现无缝滚动,我们把第一张克隆一份放在容器的末尾
-    var imgBoxLis = imgBox.getElementsByTagName('li'),
-        cloneLI = imgBoxLis[0].cloneNode(true);
-    imgBox.appendChild(cloneLI);
-
     //->动态设置一下imgBox的宽度
-    utils.css(imgBox, 'width', (bannerData.length + 1) * 1000);
+    utils.css(imgBox, 'width', bannerData.length * 1000);
 }();
 
 //=>图片延迟加载
@@ -57,33 +52,27 @@ function lazyImg(oImg) {
 //=>轮播图运动
 ~function () {
     var step = 0,//->步长:记录当前展示图片的索引
-        interval = 3000;//->时间因子:控制每隔多少MS切换到下一张图片
+        interval = 1000;//->时间因子:控制每隔多少MS切换到下一张图片
 
     var autoTimer = window.setInterval(function () {
-        if (step === 4) {//->过了3000MS再次执行定时器,此时STEP=4,说明上一次已经切换到最末尾的那一张了(经过克隆的),此时我们让容器立即回到第一张的位置,让其STEP=0
-            utils.css(imgBox, 'left', 0);
+        step++;
+        if (step >= 4) {
             step = 0;
         }
-        step++;
+        //->用300MS完成从当前图片切换到下一张图片的效果
         zhufengAnimate({
             curEle: imgBox,
             target: {
                 left: -step * 1000
             },
-            duration: 500
+            duration: 300
         });
+        //->切换到下一张图片,需要把下一张图片做一下延迟加载
         lazyImg(imgList[step]);
     }, interval);
 }();
 
-/*
- * 实现轮播图无缝滚动的原理
- * 1、数据绑定的时候，我们就把第一张克隆一份一模一样的，放在最末尾的位置（真实图片如果有四个，此时容器中存储的就是五个了）
- *
- * 2、当运动的时候，我们依然STEP++，当我们再次运动的时候，看一下上一次是否已经是最后一张了(克隆的那一张)，如果是的话，说明在往后走不了了，我们此时让容器立马回到LEFT=0的位置(真实第一张的位置)，并且让STEP=0
- *
- * 3、然后继续STEP++，运动到第二张的位置，实现了无缝衔接
- */
+
 
 
 
